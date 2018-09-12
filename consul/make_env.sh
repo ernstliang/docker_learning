c5733e8adc391f29d526096bedfd02097585607e
ernstliang <ernstliang@tencent.com>
2018-09-12 21:53:08 +0800

    更新脚本

--- consul/make_env.sh
+++ consul/make_env.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# 解压consul
+unzip consul_1.2.2_linux_amd64.zip
+
 # 创建server和agent存储数据的目录
-mkdir server1/data
-mkdir server2/data
-mkdir server3/data
-mkdir agent1/data
-mkdir agent2/data
+mkdir -p server1/data
+mkdir -p server2/data
+mkdir -p server3/data
+mkdir -p agent1/data
+mkdir -p agent2/data
