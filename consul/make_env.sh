149e6cf60250c70c2617b816386e6ddf4b27bb0e
ernstliang <ernstliang@tencent.com>
2019-03-10 11:26:54 +0800

    delete consul bin and use shell download

--- consul/make_env.sh
+++ consul/make_env.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# 下载consul安装包
+wget https://releases.hashicorp.com/consul/1.4.3/consul_1.4.3_linux_amd64.zip
+
 # 解压consul
-unzip consul_1.2.2_linux_amd64.zip
+unzip consul_1.4.3_linux_amd64.zip
 
 # 创建server和agent存储数据的目录
 mkdir -p server1/data
